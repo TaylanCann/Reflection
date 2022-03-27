7a7f0d27d8c53bf11ae8bf481d1b55eed1fac9d5
TaylanCann <taylancanh@gmail.com>
2022-03-27 03:50:53 +0300

    Add project files.

---
+++ Reflection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            var a = user.GetType();
+            Console.WriteLine(a);
+            var ab = user.GetType().GetProperties();
+            Console.WriteLine(ab[0]);
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
