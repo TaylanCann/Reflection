fb0fd71d98eaa5728f5c2ecac992c6e7609f7549
TaylanCann <taylancanh@gmail.com>
2022-03-27 04:22:21 +0300

    AddReadmeImage

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -7,10 +7,36 @@ namespace Reflection
         static void Main(string[] args)
         {
             User user = new User();
-            var a = user.GetType();
-            Console.WriteLine(a);
-            var ab = user.GetType().GetProperties();
-            Console.WriteLine(ab[0]);
+
+            //Reflection, using for get class name  
+            #region GetTypeName
+            var getTypeName = user.GetType().Name;
+            Console.WriteLine(getTypeName);
+            #endregion  
+
+            //Reflection, using for get class properties
+            #region GetTypeProperties
+            var getTypeProperties = user.GetType().GetProperties();
+            for (int i = 0; i < getTypeProperties.Length; i++)
+            {
+                Console.WriteLine(getTypeProperties[i]);
+            }
+            #endregion
+
+            //Reflection, using for get class specific property
+            #region GetTypeProperty
+            var getTypeProperty = user.GetType().GetProperty("Surname");
+            Console.WriteLine(getTypeProperty);
+            #endregion
+
+            //Reflection, using for get and set class properties
+            #region GetSetValue
+            var surname = getTypeProperty.GetValue(user, null);
+            getTypeProperty.SetValue(user, "TaylanCanHardal", null);
+            surname = getTypeProperty.GetValue(user, null);
+            Console.WriteLine(surname);
+            #endregion
+
         }
     }
 
